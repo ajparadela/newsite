5c550584a0e67b149fcfdf47951b9d8e3b6565ec
jacob <jacobaristain.13@gmail.com>
2017-12-13 23:27:39 +0800

    uploaded

--- includes/navigation.php
+++ includes/navigation.php
@@ -13,6 +13,10 @@
                     </button>
 						<a class="navbar-brand" href="index.php"><img src="img/PICTURE1.png" height="145px" style="margin-top: -45px"></a>
  					</div>
+ 					<div align="right"></data><div id="google_translate_element"></div><script type="text/javascript">
+										function googleTranslateElementInit() {
+	  									new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,en,es,id,ja,ru,th,tl,vi,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true}, 'google_translate_element'); }
+							</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script></div>
 					<div class="navbar-collapse collapse ">
 						<ul class="nav navbar-nav">
 							
